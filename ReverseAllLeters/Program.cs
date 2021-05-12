966b5cab24087b73114316bcb1a1d5d2f1fced39
Srivirinchi <srivirinchi2014@gmail.com>
2021-05-12 23:57:17 +0530

    First Commit

---
+++ ReverseAllLeters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ReverseAllLeters
+{
+    public class Sample
+    {
+        public static void reverse(char[] ch)
+        { 
+            int a = ch.Length - 1, b = 0; 
+            while (b < a)
+            {
+                if (!char.IsLetter(ch[b]))
+                    b++;
+                else if (!char.IsLetter(ch[a]))
+                    a--;
+                else
+                {
+                    char temp = ch[b];
+                    ch[b] = ch[a];
+                    ch[a] = temp;
+                    b++;
+                    a--;
+                }
+            }
+        }
+        public static void Main()
+        {
+            String str = "ab1c2de345f";
+            char[] ArrayList = str.ToCharArray();
+
+            Console.WriteLine("Input string: " + str);
+            reverse(ArrayList);
+            String reverseStr = new String(ArrayList);
+
+            Console.WriteLine("Output string: " + reverseStr);
+        }
+    }
+}
+
+
